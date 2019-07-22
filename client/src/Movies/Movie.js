d5f23b9bd3aa0f1d365c0f5e83ec61d357b36625
Areum Jo <areumjo1@gmail.com>
2019-07-22 16:11:29 -0400

    Part2: Each movie has their own URL

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,27 +2,31 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
+  
   const [movie, setMovie] = useState();
- 
-  useEffect(() => {
-    const id = 1;
+  
+  useEffect(() => { 
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
+    fetchMovie();
+  }, []);
 
-    const fetchMovie = id => {
-      axios
-        .get(`http://localhost:5000/api/movies/${id}`)
-        .then(response => {
-          setMovie(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    };
-  });
+  const fetchMovie = () => {
+    axios
+      .get(`http://localhost:5000/api/movies/${props.match.params.id}`)
+      .then(response => {
+        setMovie(response.data);
+        console.log('data: ', response.data);
+      })
+      .catch(error => {
+        console.error('Movie.js error:', error);
+      });
+  };
+  console.log(movie);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // saveMovie = () => {
+  // const saveMovie = () => {
   //   const addToSavedList = props.addToSavedList;
   //   addToSavedList(movie)
   // }
